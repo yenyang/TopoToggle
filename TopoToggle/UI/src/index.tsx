fece666c2a7f7e9c4387b94d3126db6bfb52c8ba
yenyang <yenyang19@pm.me>
2026-02-20 21:02:54 -0800

    Add game setting to control wear the topo toggle is

--- TopoToggle/UI/src/index.tsx
+++ TopoToggle/UI/src/index.tsx
@@ -5,6 +5,7 @@ import mod from "../mod.json";
 import { ToolOptionsVisibility } from "mods/ToolOptionsVisible/toolOptionsVisible";
 import { TopLeftTopoToggleComponent } from "mods/TopLeftTopoToggleComponent/TopLeftTopoToggleComponent";
 import { TopRightTopoToggleComponent } from "mods/TopRightTopoToggleComponent/TopRightTopoToggleComponent";
+import { BottomRightTopoToggleComponent } from "mods/BottomRightTopoToggleComponent/BottomRightTopoToggleComponent";
 
 const register: ModRegistrar = (moduleRegistry) => {
       // The vanilla component resolver is a singleton that helps extrant and maintain components from game that were not specifically exposed.
@@ -16,6 +17,7 @@ const register: ModRegistrar = (moduleRegistry) => {
      moduleRegistry.append('Editor', TopoPanelComponent);     
      moduleRegistry.append('GameTopLeft', TopLeftTopoToggleComponent);
      moduleRegistry.append('GameTopRight', TopRightTopoToggleComponent);
+     moduleRegistry.append('GameBottomRight', BottomRightTopoToggleComponent);
 
      // Ensures tool option is visible for water tool in the editor.
       moduleRegistry.extend("game-ui/game/components/tool-options/tool-options-panel.tsx", 'useToolOptionsVisible', ToolOptionsVisibility);
--- TopoToggle/UI/src/mods/BottomRightTopoToggleComponent/BottomRightTopoToggleComponent.tsx
+++ TopoToggle/UI/src/mods/BottomRightTopoToggleComponent/BottomRightTopoToggleComponent.tsx
@@ -2,10 +2,11 @@ import { Button } from "cs2/ui";
 import ContourLinesSrc from "../../images/ContourLines.svg";
 import { bindValue, trigger, useValue } from "cs2/api";
 import mod from "../../../mod.json";
-import styles from "../TopLeftTopoToggleComponent/topBoundStyles.module.scss";
+import styles from "../BottomRightTopoToggleComponent/bottomRightStyles.module.scss";
 import classNames from "classnames";
 import { useLocalization } from "cs2/l10n";
 import locale from "../lang/en-US.json";
+import { getModule } from "cs2/modding";
 import { GameToggleOptions } from "Domain/GameToggleOptions";
 
 // These establishes the binding with C# side. Without C# side game ui will crash.
@@ -15,34 +16,39 @@ const TerrainElevation$ = bindValue(mod.id, "TerrainElevation", ":???");
 const GameToggleOption$ = bindValue(mod.id, "GameToggleOption", GameToggleOptions.FloatingPanel);
 const HideTopoTogglePanel$ = bindValue(mod.id, "HideTopoTogglePanel", false);
 
-export const TopRightTopoToggleComponent = () => 
+const rightMenuStyles = getModule("game-ui/game/components/right-menu/right-menu.module.scss", "classes");
+
+const rightMenuButtonStyles = getModule("game-ui/game/components/right-menu/right-menu-button.module.scss", "classes");
+
+export const BottomRightTopoToggleComponent = () => 
 {
-    const GameToggleOption = useValue(GameToggleOption$); 
     const ForceContourLines = useValue(ForceContourLines$);    
     const ShowTerrainHitPosition = useValue(ShowTerrainElevation$);
     const TerrainElevation = useValue(TerrainElevation$);
+    const GameToggleOption = useValue(GameToggleOption$);    
     const HideTopoTogglePanel = useValue(HideTopoTogglePanel$);
 
     const { translate } = useLocalization();
     
     return     (
         <>
-            {GameToggleOption == GameToggleOptions.TopRight && !HideTopoTogglePanel && (
-                <div className={styles.columnGroup}>
-                    <>
-                        <Button
-                            src={ContourLinesSrc}
-                            variant="floating"
-                            className={classNames({ [styles.selected]: ForceContourLines }, styles.toggle)}
-                            onSelect={() => trigger(mod.id, "ToggleContourLines")}
-                        />
-                        { ShowTerrainHitPosition && 
+            {GameToggleOption == GameToggleOptions.BottomRight && !HideTopoTogglePanel && (
+                <div className={classNames(styles.bottomRightMargins, ShowTerrainHitPosition? styles.overrideHeight: "")}>
+                    <div className={rightMenuStyles.item}>
+                            <Button
+                                src={ContourLinesSrc}
+                                variant="icon"
+                                className={classNames( ForceContourLines? "selected": "" , rightMenuButtonStyles.button)}
+                                onSelect={() => trigger(mod.id, "ToggleContourLines")}
+                            />                
+                        
+                    </div>
+                    { ShowTerrainHitPosition && 
                             <div className={classNames(styles.smallSize, styles.absolutePosition)}>{ translate("TopoToggle.Text_Label_[ElevationAbbreviation]" ,locale["TopoToggle.Text_Label_[ElevationAbbreviation]"])+ TerrainElevation}</div>
-                        }
-                    </> 
+                    }
                 </div>
             )}
-       </>
+        </>
     );
 
 }
